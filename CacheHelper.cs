38abe210365baea997b345d2c9dee10d6c69983d
wsq003 <wsq003@gmail.com>
2021-07-13 14:15:59 +0800

    Update CacheHelper.cs
    WithCacheRefresh support async. Add volatile to isRefreshing.

--- CacheHelper.cs
+++ CacheHelper.cs
@@ -1,71 +1,119 @@
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Caching;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace WSQ.Common
+namespace Common.Utils
 {
 	/// <summary>
-	/// 用MemoryCache缓存函数的执行结果。可以减少对db和redis的读取，也可以减少任何其他函数的执行。
-	/// 用例：var preData = CacheHelper.WithCache("userstock_GetUSPreDataTradingCode_" + stockCode, ()=>Redis.QuoteData.GetUSPreDataTradingCode(stockCode), 5000);
+	/// 
 	/// </summary>
 	public static class CacheHelper
 	{
-		private static MemoryCache mc = new MemoryCache(Guid.NewGuid().ToString());
+		private static MemoryCache mc = new MemoryCache(new MemoryCacheOptions());
+
+		private static string nullstr = "ajktNullHolder";
 
 		/// <summary>
-		/// 用MemoryCache缓存getData的结果。Func部分可以 ()=>getData(a,b,c)
+		/// 
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
-		/// <param name="key">注意key要够独特</param>
-		/// <param name="getData"></param>
+		/// <param name="key"></param>
+		/// <param name="getData">can ()=>getData(a,b,c)</param>
 		/// <param name="arg"></param>
 		/// <param name="cacheMilli"></param>
 		/// <returns></returns>
-		public static T WithCache<T>(string key, Func<T> getData, double cacheMilli = 1000) where T : new()
+		public static T? WithCache<T>(string key, Func<T> getData, double cacheMilli = 1000, bool cacheNullResult = false)
 		{
 			object obj = mc.Get(key);
 			if (obj != null)
 			{
+				if (obj is string)
+				{
+					if (obj as string == nullstr)
+					{
+						return default(T);
+					}
+				}
 				return (T)obj;
 			}
 			else
 			{
-				T dd = getData();
+				T? dd = getData();
 				if (dd == null)
 				{
-					dd = new T();
+					if (cacheNullResult)
+					{
+						mc.Set(key, nullstr, DateTime.Now.AddMilliseconds(cacheMilli));
+						dd = default(T);
+					}
 				}
-				mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				else
+				{
+					mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				}
 				return dd;
 			}
 		}
 
+		public static async Task<T?> WithCacheAsync<T>(string key, Func<Task<T?>> getData, double cacheMilli = 1000, bool cacheNullResult = false)
+		{
+			object obj = mc.Get(key);
+			if (obj != null)
+			{
+				if (obj is string)
+				{
+					if (obj as string == nullstr)
+					{
+						return default(T);
+					}
+				}
+				return (T)obj;
+			}
+			else
+			{
+				var dd = await getData();
+				if (dd == null)
+				{
+					if (cacheNullResult)
+					{
+						mc.Set(key, nullstr, DateTime.Now.AddMilliseconds(cacheMilli));
+						dd = default(T);
+					}
+				}
+				else
+				{
+					mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				}
+				return dd;
+			}
+		}
+
+		public static void RemoveCache(string key)
+		{
+			mc.Remove(key);
+		}
+
 		private class CacheInfo
 		{
-			public string key;
+			public string key = "";
 			public DateTime lastCacheTime;
 			public double cacheMilli;
-			/// <summary>
-			/// 控制并发程度，不需要严格的线程安全
-			/// </summary>
-			public bool isRefreshing;
+
+			public volatile bool isRefreshing;
 
 			/// <summary>
-			/// 是否需要提前刷新
+			/// 
 			/// </summary>
 			/// <returns></returns>
 			public bool NeedPredicateRefresh()
 			{
 				var milliExist = (DateTime.Now - this.lastCacheTime).TotalMilliseconds;
 
-				//正在提前刷新的别再提前刷新。
-				//缓存太短的数据没必要提前刷新。
-				//快过期了才提前刷新。
 				bool needRefresh = !isRefreshing && this.cacheMilli > 500 && milliExist > (this.cacheMilli * 0.8);
 				return needRefresh;
 			}
@@ -73,16 +121,15 @@ namespace WSQ.Common
 		private static ConcurrentDictionary<string, CacheInfo> dicCacheInfo = new ConcurrentDictionary<string, CacheInfo>();
 
 		/// <summary>
-		/// 用MemoryCache缓存getData的结果。Func部分可以 ()=>getData(a,b,c)。
-		/// 在缓存快过期的时候，按需启动线程去提前刷新缓存，以尽量避免Func太慢导致的偶发响应变慢。
+		/// 
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
-		/// <param name="key">注意key要够独特</param>
-		/// <param name="getData"></param>
+		/// <param name="key"></param>
+		/// <param name="getData">can be ()=>getData(a,b,c)</param>
 		/// <param name="arg"></param>
 		/// <param name="cacheMilli"></param>
 		/// <returns></returns>
-		public static T WithCacheRefresh<T>(string key, Func<T> getData, double cacheMilli = 1000) where T : new()
+		public static T? WithCacheRefresh<T>(string key, Func<T> getData, double cacheMilli = 1000)
 		{
 			var info = dicCacheInfo.GetOrAdd(key, new CacheInfo()
 			{
@@ -95,10 +142,8 @@ namespace WSQ.Common
 			object obj = mc.Get(key);
 			if (obj != null)
 			{
-				//cache存在，看看要不要提前刷新
 				if (info.NeedPredicateRefresh())
 				{
-					//立刻设置一下isRefreshing，避免起太多线程
 					info.isRefreshing = true;
 
 					Thread th = new Thread(new ThreadStart(() => CheckRefreshCache(key, getData, cacheMilli)));
@@ -107,16 +152,27 @@ namespace WSQ.Common
 					th.Start();
 				}
 
+				if (obj is string)
+				{
+					if (obj as string == nullstr)
+					{
+						return default(T);
+					}
+				}
 				return (T)obj;
 			}
 			else
 			{
-				T dd = getData();
+				T? dd = getData();
 				if (dd == null)
 				{
-					dd = new T();
+					mc.Set(key, nullstr, DateTime.Now.AddMilliseconds(cacheMilli));
+					dd = default(T);
 				}
-				mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				else
+				{
+					mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				}
 
 				info.lastCacheTime = DateTime.Now;
 				info.cacheMilli = cacheMilli;
@@ -132,9 +188,9 @@ namespace WSQ.Common
 		/// <param name="key"></param>
 		/// <param name="getData"></param>
 		/// <param name="cacheMilli"></param>
-		private static void CheckRefreshCache<T>(string key, Func<T> getData, double cacheMilli) where T : new()
+		private static void CheckRefreshCache<T>(string key, Func<T> getData, double cacheMilli)
 		{
-			dicCacheInfo.TryGetValue(key, out CacheInfo info);
+			dicCacheInfo.TryGetValue(key, out CacheInfo? info);
 			if (info == null)
 			{
 				return;
@@ -142,12 +198,99 @@ namespace WSQ.Common
 
 			try
 			{
-				T dd = getData();
+				T? dd = getData();
 				if (dd == null)
 				{
-					dd = new T();
+					mc.Set(key, nullstr, DateTime.Now.AddMilliseconds(cacheMilli));
+					dd = default(T);
 				}
-				mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				else
+				{
+					mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				}
+
+				info.lastCacheTime = DateTime.Now;
+				info.cacheMilli = cacheMilli;
+			}
+			catch (Exception)
+			{
+			}
+			info.isRefreshing = false;
+		}
+
+		public static async Task<T?> WithCacheRefreshAsync<T>(string key, Func<Task<T?>> getData, double cacheMilli = 1000)
+		{
+			var info = dicCacheInfo.GetOrAdd(key, new CacheInfo()
+			{
+				key = key,
+				lastCacheTime = new DateTime(),
+				cacheMilli = cacheMilli,
+				isRefreshing = false
+			});
+
+			object obj = mc.Get(key);
+			if (obj != null)
+			{
+				if (info.NeedPredicateRefresh())
+				{
+					info.isRefreshing = true;
+
+					Thread th = new Thread(new ThreadStart(async () => await CheckRefreshCacheAsync(key, getData, cacheMilli)));
+					th.Name = "WithCacheConcurrent_refresh";
+					th.IsBackground = true;
+					th.Start();
+				}
+
+				if (obj is string)
+				{
+					if (obj as string == nullstr)
+					{
+						return default(T);
+					}
+				}
+				return (T)obj;
+			}
+			else
+			{
+				T? dd = await getData();
+				if (dd == null)
+				{
+					mc.Set(key, nullstr, DateTime.Now.AddMilliseconds(cacheMilli));
+					dd = default(T);
+				}
+				else
+				{
+					mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				}
+
+				info.lastCacheTime = DateTime.Now;
+				info.cacheMilli = cacheMilli;
+
+				return dd;
+			}
+		}
+
+
+		private static async Task CheckRefreshCacheAsync<T>(string key, Func<Task<T>> getData, double cacheMilli)
+		{
+			dicCacheInfo.TryGetValue(key, out CacheInfo? info);
+			if (info == null)
+			{
+				return;
+			}
+
+			try
+			{
+				T? dd = await getData();
+				if (dd == null)
+				{
+					mc.Set(key, nullstr, DateTime.Now.AddMilliseconds(cacheMilli));
+					dd = default(T);
+				}
+				else
+				{
+					mc.Set(key, dd, DateTime.Now.AddMilliseconds(cacheMilli));
+				}
 
 				info.lastCacheTime = DateTime.Now;
 				info.cacheMilli = cacheMilli;
